14b37108ea6b70386db230ee6c992b76ce7942b0
Jeof Petty <jeofpetty@icloud.com>
2023-02-04 16:57:11 -0500

    sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,18 +7,18 @@ app.config['SECRET_KEY'] = 'chickens'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 @app.route('/')
 def index():
     return render_template('index.html', survey=satisfaction_survey)
 
 @app.route('/start', methods=["POST"])
 def start_question():
+    session["responses"] = []
     return redirect('/questions/0')
 
 @app.route('/questions/<int:idx>')
 def get_question(idx):
+    responses = session['responses']
     if len(responses) != idx:
         flash('Cannot access invalid question...')
         return redirect(f'/questions/{len(responses)}')
@@ -27,7 +27,10 @@ def get_question(idx):
 
 @app.route('/answers', methods=['POST'])
 def store_answers():
-    responses.append(request.form['answers'])
+    responses = session['responses']
+    answers = request.form['answers']
+    responses.append(answers)
+    session['responses'] = responses
     if len(responses) == len(satisfaction_survey.questions):
         return redirect('/thanks')
     return redirect(f'/questions/{len(responses)}')
